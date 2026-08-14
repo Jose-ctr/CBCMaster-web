9999017ffffa11d4d5dd76442bd30164ee4e8326
Joseph | Founder ThinkPlus <146562490+Jose-ctr@users.noreply.github.com>
2026-08-14 22:58:52 +0300

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,74 @@
-const CACHE_NAME = 'cbc-master-v1';
+const CACHE_NAME = "cbc-master-v2";
 
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './questions.json'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            if (
+              !networkResponse ||
+              networkResponse.status !== 200 ||
+              networkResponse.type === "opaque"
+            ) {
+              return networkResponse;
+            }
+
+            const responseToCache =
+              networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(
+                  event.request,
+                  responseToCache
+                );
+              });
+
+            return networkResponse;
+          })
+          .catch(() => {
+            return caches.match("./index.html");
+          });
+
+      })
   );
 });
