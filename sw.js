55c29f08f714e1e8e96564707197f3bb931513d2
Joseph | Founder ThinkPlus <146562490+Jose-ctr@users.noreply.github.com>
2026-09-21 09:33:24 +0300

    Update service worker cache for external script.js

--- sw.js
+++ sw.js
@@ -1,75 +1,75 @@
-const CACHE_NAME = "cbc-master-v2";
+const CACHE_NAME = "cbc-master-v3";
 
 const FILES_TO_CACHE = [
-"./",
-"./index.html",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(FILES_TO_CACHE))
-.then(() => self.skipWaiting())
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(
-keys
-.filter(key => key !== CACHE_NAME)
-.map(key => caches.delete(key))
-)
-).then(() => self.clients.claim())
-);
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-if (event.request.method !== "GET") {
-return;
-}
+  if (event.request.method !== "GET") {
+    return;
+  }
 
-event.respondWith(
-caches.match(event.request)
-.then(cachedResponse => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
 
-if (cachedResponse) {  
-      return cachedResponse;  
-    }  
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-    return fetch(event.request)  
-      .then(networkResponse => {  
+        return fetch(event.request)
+          .then(networkResponse => {
 
-        if (  
-          !networkResponse ||  
-          networkResponse.status !== 200 ||  
-          networkResponse.type === "opaque"  
-        ) {  
-          return networkResponse;  
-        }  
+            if (
+              !networkResponse ||
+              networkResponse.status !== 200 ||
+              networkResponse.type === "opaque"
+            ) {
+              return networkResponse;
+            }
 
-        const responseToCache =  
-          networkResponse.clone();  
+            const responseToCache =
+              networkResponse.clone();
 
-        caches.open(CACHE_NAME)  
-          .then(cache => {  
-            cache.put(  
-              event.request,  
-              responseToCache  
-            );  
-          });  
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(
+                  event.request,
+                  responseToCache
+                );
+              });
 
-        return networkResponse;  
-      })  
-      .catch(() => {  
-        return caches.match("./index.html");  
-      });  
+            return networkResponse;
+          })
+          .catch(() => {
+            return caches.match("./index.html");
+          });
 
-  })
-
-);
+      })
+  );
 });
